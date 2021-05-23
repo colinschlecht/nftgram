7f9061a9e295b0c036eae92aab8efe8ec5fc2027
Colin Schlecht <schlechtcolin@yahoo.com>
2021-05-22 23:36:52 -0700

    gets abi and adds to abi src folder

--- scripts/get-abi.js
+++ scripts/get-abi.js
@@ -1,1 +1,5 @@
-const contract = require("../artifacts/contracts/HelloWorldNFT.sol/HelloWorldNFT.json"); 
+const contract = require("../artifacts/contracts/NFTgramIO.sol/NFTgramIO.json"); 
+
+console.log(JSON.stringify(contract.abi));
+
+//node scripts/get-abi.js 
