8f859dfbc4f64af79caacc059d9395d728d39940
Colin Schlecht <schlechtcolin@yahoo.com>
2021-06-03 21:08:19 -0700

    all tests passing

--- test/Token.test.js
+++ test/Token.test.js
@@ -3,14 +3,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 require("@nomiclabs/hardhat-ganache");
 
-// const ganache = require("ganache-cli");
-// const Web3 = require("web3");
-// const web3 = new Web3(ganache.provider());
-
-// const contractABI = require("../src/abi/NFTgramIOABI.json");
-// const contractAddress = "0x3032107eAcD70a6590b24A1FD8A53Ecf4E9c3692"
-
-//! npx hardhat --network ganache test
+//! to run the test, run the line of code below in your terminal
+// npx hardhat --network ganache test
 
 let tokenURI;
 let accounts;
@@ -107,150 +101,150 @@ describe("SaleFactory contract", function () {
     assert(saleFactory);
   });
 
-  // it("has methods associated with the contract.", async function () {
-  //   assert(Object.keys(saleFactory.functions));
-  // });
+  it("has methods associated with the contract.", async function () {
+    assert(Object.keys(saleFactory.functions));
+  });
 
-  // it("can deploy a Sale Contract and provide the contract address", async function () {
-  //   await saleFactory.createSale(contract.address, 1, 1);
-  //   await saleFactory.createSale(contract.address, 2, 1);
-  //   trades = await saleFactory.getSales();
-  //   assert.strictEqual(trades.length, 2);
-  // });
+  it("can deploy a Sale Contract and provide the contract address", async function () {
+    await saleFactory.createSale(contract.address, 1, 1);
+    await saleFactory.createSale(contract.address, 2, 1);
+    trades = await saleFactory.getSales();
+    assert.strictEqual(trades.length, 2);
+  });
 });
 
 // //!Deployed Sale contract
-// describe("Deployed Sale contract", function () {
-//   beforeEach(async function () {
-//     //nft
-//     accounts = await ethers.provider.listAccounts();
-//     signer0 = await ethers.provider.getSigner(accounts[0]);
-//     signer1 = await ethers.provider.getSigner(accounts[1]);
-//     testContract = await ethers.getContractFactory("NFTgramIO");
-//     contract = await testContract.deploy();
-//     deployNFT = await contract.mintNFT(accounts[0], tokenURI);
-//     deployNFT2 = await contract.mintNFT(accounts[0], tokenURI);
-//     //saleFactory
-//     saleFactoryPre = await ethers.getContractFactory("SaleFactory");
-//     saleFactory = await saleFactoryPre.deploy();
-//     //approve contract interraction
-//     await saleFactory.createSale(contract.address, 1, 1);
-//     await saleFactory.createSale(contract.address, 2, 1);
-//     trades = await saleFactory.getSales();
-//     await contract.approve(trades[0], 1, { from: accounts[0] });
-//     await contract.approve(trades[1], 2, { from: accounts[0] });
-//     //get sales contract
-//     saleContract = await ethers.getContractAt("Sale", trades[0]);
-//     saleContract2 = await ethers.getContractAt("Sale", trades[1]);
-//   });
+describe("Deployed Sale contract", function () {
+  beforeEach(async function () {
+    //nft
+    accounts = await ethers.provider.listAccounts();
+    signer0 = await ethers.provider.getSigner(accounts[0]);
+    signer1 = await ethers.provider.getSigner(accounts[1]);
+    testContract = await ethers.getContractFactory("NFTgramIO");
+    contract = await testContract.deploy();
+    deployNFT = await contract.mintNFT(accounts[0], tokenURI);
+    deployNFT2 = await contract.mintNFT(accounts[0], tokenURI);
+    //saleFactory
+    saleFactoryPre = await ethers.getContractFactory("SaleFactory");
+    saleFactory = await saleFactoryPre.deploy();
+    //approve contract interraction
+    await saleFactory.createSale(contract.address, 1, 1);
+    await saleFactory.createSale(contract.address, 2, 1);
+    trades = await saleFactory.getSales();
+    await contract.approve(trades[0], 1, { from: accounts[0] });
+    await contract.approve(trades[1], 2, { from: accounts[0] });
+    //get sales contract
+    saleContract = await ethers.getContractAt("Sale", trades[0]);
+    saleContract2 = await ethers.getContractAt("Sale", trades[1]);
+  });
 
-//   it("can display it's summary, and show status of pending", async function () {
-//     summary = await saleContract.getSummary();
-//     const status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Pending");
-//   });
+  it("can display it's summary, and show status of pending", async function () {
+    summary = await saleContract.getSummary();
+    const status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Pending");
+  });
 
-//   it("openTrade can only be called by the owner", async function () {
-//     //this is reverted by ERC721 contract
-//     await expect(saleContract.connect(signer1).openTrade({ from: accounts[1] }))
-//       .to.be.reverted;
-//   });
+  it("openTrade can only be called by the owner", async function () {
+    //this is reverted by ERC721 contract
+    await expect(saleContract.connect(signer1).openTrade({ from: accounts[1] }))
+      .to.be.reverted;
+  });
 
-//   it("openTrade function can place an item for sale, and move token to escrow, and list status as 'Open'", async function () {
-//     await saleContract.openTrade();
-//     const ownerAddress = await contract.ownerOf(1);
-//     const contractAddress = saleContract.address;
-//     assert.strictEqual(ownerAddress, contractAddress);
-//     summary = await saleContract.getSummary();
-//     const status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Open");
-//   });
+  it("openTrade function can place an item for sale, and move token to escrow, and list status as 'Open'", async function () {
+    await saleContract.openTrade();
+    const ownerAddress = await contract.ownerOf(1);
+    const contractAddress = saleContract.address;
+    assert.strictEqual(ownerAddress, contractAddress);
+    summary = await saleContract.getSummary();
+    const status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Open");
+  });
 
-//
 
-//   it("cancelTrade can cancel an item for sale, move the item back to the seller's ownership, and list the status as 'Cancelled'", async function () {
-//     await saleContract.openTrade();
-//     const ownerAddress = await contract.ownerOf(1);
-//     const contractAddress = saleContract.address;
-//     assert.strictEqual(ownerAddress, contractAddress);
-//     await saleContract.cancelTrade();
-//     const ownerAddressAfter = await contract.ownerOf(1);
-//     assert.strictEqual(ownerAddressAfter, accounts[0]);
-//     summary = await saleContract.getSummary();
-//     const status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Cancelled");
-//   });
 
-//   it("cancelTrade will not cancel a sale that is outside of 'Open' status.", async function () {
-//     //Pending transaction
-//     summary = await saleContract.getSummary();
-//     status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Pending");
-//     await expect(saleContract.cancelTrade()).to.be.revertedWith(
-//       "Trade is not Open."
-//     );
+  it("cancelTrade can cancel an item for sale, move the item back to the seller's ownership, and list the status as 'Cancelled'", async function () {
+    await saleContract.openTrade();
+    const ownerAddress = await contract.ownerOf(1);
+    const contractAddress = saleContract.address;
+    assert.strictEqual(ownerAddress, contractAddress);
+    await saleContract.cancelTrade();
+    const ownerAddressAfter = await contract.ownerOf(1);
+    assert.strictEqual(ownerAddressAfter, accounts[0]);
+    summary = await saleContract.getSummary();
+    const status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Cancelled");
+  });
 
-//     //Cancelled transaction
-//     await saleContract.openTrade();
-//     await saleContract.cancelTrade();
-//     summary = await saleContract.getSummary();
-//     status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Cancelled");
-//     await expect(saleContract.cancelTrade()).to.be.revertedWith(
-//       "Trade is not Open."
-//     );
+  it("cancelTrade will not cancel a sale that is outside of 'Open' status.", async function () {
+    //Pending transaction
+    summary = await saleContract.getSummary();
+    status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Pending");
+    await expect(saleContract.cancelTrade()).to.be.revertedWith(
+      "Trade is not Open."
+    );
 
-//     //Executed transaction
-//     await saleContract2.openTrade();
-//     await saleContract2.connect(signer1).purchaseToken({ value: 1, from: accounts[1] })
-//     summary = await saleContract2.getSummary();
-//     status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Executed");
-//     await expect(saleContract.cancelTrade()).to.be.revertedWith(
-//       "Trade is not Open."
-//     );
-//   });
+    //Cancelled transaction
+    await saleContract.openTrade();
+    await saleContract.cancelTrade();
+    summary = await saleContract.getSummary();
+    status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Cancelled");
+    await expect(saleContract.cancelTrade()).to.be.revertedWith(
+      "Trade is not Open."
+    );
 
-//   it("cancelPending can move a pending (yet to be Opened) Sale contract to Cancelled", async function () {
-//     const ownerAddress = await contract.ownerOf(1);
-//     assert.strictEqual(ownerAddress, accounts[0]);
-//     await saleContract.cancelPending();
-//     const summary = await saleContract.getSummary();
-//     const status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Cancelled");
-//   });
+    //Executed transaction
+    await saleContract2.openTrade();
+    await saleContract2.connect(signer1).purchaseToken({ value: 1, from: accounts[1] })
+    summary = await saleContract2.getSummary();
+    status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Executed");
+    await expect(saleContract.cancelTrade()).to.be.revertedWith(
+      "Trade is not Open."
+    );
+  });
 
-//   it("cancelPending will not cancel a sale that is outside of 'Pending' status.", async function () {
-//     //Open transaction
-//     await saleContract.openTrade();
-//     summary = await saleContract.getSummary();
-//     status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Open");
-//     await expect(saleContract.cancelPending()).to.be.revertedWith(
-//       "Trade is not Pending."
-//     );
+  it("cancelPending can move a pending (yet to be Opened) Sale contract to Cancelled", async function () {
+    const ownerAddress = await contract.ownerOf(1);
+    assert.strictEqual(ownerAddress, accounts[0]);
+    await saleContract.cancelPending();
+    const summary = await saleContract.getSummary();
+    const status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Cancelled");
+  });
 
-//     //Cancelled transaction
-//     await saleContract.cancelTrade();
-//     summary = await saleContract.getSummary();
-//     status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Cancelled");
-//     await expect(saleContract.cancelPending()).to.be.revertedWith(
-//       "Trade is not Pending."
-//     );
+  it("cancelPending will not cancel a sale that is outside of 'Pending' status.", async function () {
+    //Open transaction
+    await saleContract.openTrade();
+    summary = await saleContract.getSummary();
+    status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Open");
+    await expect(saleContract.cancelPending()).to.be.revertedWith(
+      "Trade is not Pending."
+    );
 
-//     //Executed transaction
-//     await saleContract2.openTrade();
-//     await saleContract2.connect(signer1).purchaseToken({ value: 1, from: accounts[1] })
-//     summary = await saleContract2.getSummary();
-//     status = ethers.utils.parseBytes32String(summary[4]);
-//     assert.strictEqual(status, "Locked");
-//     await expect(saleContract.cancelPending()).to.be.revertedWith(
-//       "Trade is not Pending."
-//     );
+    //Cancelled transaction
+    await saleContract.cancelTrade();
+    summary = await saleContract.getSummary();
+    status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Cancelled");
+    await expect(saleContract.cancelPending()).to.be.revertedWith(
+      "Trade is not Pending."
+    );
 
-//   });
-// });
+    //Executed transaction
+    await saleContract2.openTrade();
+    await saleContract2.connect(signer1).purchaseToken({ value: 1, from: accounts[1] })
+    summary = await saleContract2.getSummary();
+    status = ethers.utils.parseBytes32String(summary[4]);
+    assert.strictEqual(status, "Executed");
+    await expect(saleContract.cancelPending()).to.be.revertedWith(
+      "Trade is not Pending."
+    );
+
+  });
+});
 
 //!PurchaseToken function
 describe("Purchase Token function", function () {
@@ -276,7 +270,6 @@ describe("Purchase Token function", function () {
     saleContract = await ethers.getContractAt("Sale", trades[0]);
     saleContract2 = await ethers.getContractAt("Sale", trades[1]);
     //open trade
-    await saleContract.openTrade();
     await saleContract2.openTrade();
   });
 
@@ -341,12 +334,14 @@ describe("Purchase Token function", function () {
     assert(balanceBefore._hex != balanceAfter._hex);
   });
   it("will transfer NFT to the buyer's account", async function () {
-    let balanceBefore = await ethers.provider.getBalance(accounts[0]);
-    await saleContract2
+    let ownerAddress = await contract.ownerOf(1);
+    assert.strictEqual(ownerAddress, accounts[0])
+    await saleContract.openTrade()
+    await saleContract
       .connect(signer1)
       .purchaseToken({ value: 1, from: accounts[1] });
-    let balanceAfter = await ethers.provider.getBalance(accounts[0]);
-    assert(balanceBefore._hex != balanceAfter._hex);
+    ownerAddress = await contract.ownerOf(1);
+    assert.strictEqual(ownerAddress, accounts[1])
   });
 });
 
