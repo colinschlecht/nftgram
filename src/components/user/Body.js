66b0b5ce64229f142a4d55199f3ee2a866e7db8b
Colin Schlecht <schlechtcolin@yahoo.com>
2021-06-10 16:50:41 -0700

    updates profile page body responsiveness

--- src/components/user/Body.js
+++ src/components/user/Body.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ProfileArtCard from "./ProfileArtCard";
 
-const Body = ({user}) => {
+const Body = ({ user }) => {
   return (
-    <>
-    {user.arts ? user.arts.map(art => <ProfileArtCard key={art.id} art={art}/>) : null}
-    </>
+    <div className="outer body container">
+      <div className="body container">
+        {user.arts
+          ? user.arts.map((art) => <ProfileArtCard key={art.id} art={art} />)
+          : null}
+      </div>
+    </div>
   );
 };
 
-export default Body
+export default Body;
--- src/components/user/ProfileArtCard.js
+++ src/components/user/ProfileArtCard.js
@@ -1,38 +1,41 @@
 import React from "react";
-import { Image, Card } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const ProfileArtCard = ({ art }) => {
+  const getDate = () => {
+    let ms = (Date.now() - Date.parse(art.created_at)) / 1000 / 60 / 60 / 24;
+    if (ms >= 1) {
+      return Math.floor(ms) + " days ago";
+    } else if (ms * 24 >= 1) {
+      return Math.floor(ms * 24) + " hours ago";
+    } else if (ms * 24 < 1) {
+      return Math.floor(ms * 24 * 60) + " minutes ago";
+    }
+  };
 
-
-const getDate = () => {
-  let ms = (Date.now() - Date.parse(art.created_at)) / 1000 / 60 / 60 / 24 
-  if(ms >= 1){
-    return Math.floor(ms) + " days ago"
-  } else if(ms * 24 >= 1){
-    return Math.floor(ms * 24) + " hours ago"
-  } else if(ms * 24 < 1) {
-    return Math.floor(ms * 24 * 60) + " minutes ago"
-  }
-}
-
-// const date = art.created_at.split("T")[0]
   return (
-    <Card>
-      <Card.Content>
+    <div className="ui card body">
+      <div className="content">
         <Link
           id={art.id}
           key={art.id + "u"}
           to={{ pathname: `/art/show/${art.id}` }}
         >
-          <Image alat={art.description} src={`https://ipfs.io/ipfs/${art.cid}`} fluid />
+          <div className="body image container">
+            <img
+              className="body art image"
+              alt={art.description}
+              src={`https://ipfs.io/ipfs/${art.cid}`}
+            />
+          </div>
         </Link>
+        <div className="body text content"></div>
         <h5>{art.caption}</h5>
-        <Card.Meta>
-        <span className='date'>Posted {getDate()} </span>
-      </Card.Meta>
-      </Card.Content>
-      </Card>
+        <div className="meta">
+          <span className="date">Posted {getDate()} </span>
+        </div>
+      </div>
+    </div>
   );
 };
 
