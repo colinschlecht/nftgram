6fb72c03ca7ab5cd99900934867d2bb4fda7d8e4
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-02 12:48:48 -0700

    adds avatar drop zone for user profile photo

---
+++ src/components/user/AvatarDrop.js
@@ -0,0 +1,48 @@
+import React, { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
+
+const AvatarDrop = () => {
+  const [file, setfile] = useState(input.value);
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      input.onChange(acceptedFiles);
+      const upload = new File(acceptedFiles, acceptedFiles[0].name, {
+        type: acceptedFiles[0].type,
+      });
+      const reader = new FileReader();
+      reader.readAsDataURL(upload);
+      reader.onloadend = () => {
+        const data = reader.result;
+        setfile(data);
+      };
+    },
+    [input]
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const render =
+    file.length !== 0 ? (
+      <img alt="uploaded art" src={file}></img>
+    ) : (
+      <Segment placeholder>
+        <Header icon>
+          <Icon name="camera" />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag or click to select files</p>
+          )}
+        </Header>
+      </Segment>
+    );
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {render}
+    </div>
+  );
+};
+
+export default AvatarDrop;
