aeeaec47b4fb57fd62eb7e66cd01fc602c108e79
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-05 01:33:48 -0700

    updates capitalization for title of "ui reducer"

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
-import uiReducer from "./UIReducer";
+import uiReducer from "./uiReducer";
 import artReducer from "./artReducer";
 import metaMaskReducer from "./metaMaskReducer";
 
