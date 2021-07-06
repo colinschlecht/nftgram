e6b3ac6e127b275aa476c5a56b1f0b24e56f74dc
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-06 02:03:29 -0700

    adds web3 component

---
+++ src/utils/web3.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+export default web3;
