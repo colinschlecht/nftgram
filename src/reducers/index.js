7a58bf4b5b039c9086b25f7486401dbbd97ad7d5
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-12 22:05:25 -0700

    updates reducer file name for UI

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
+import UIReducer from "./UIReducer";
 import userReducer from "./userReducer";
-import uiReducer from "./uiReducer";
 import artReducer from "./artReducer";
 import metaMaskReducer from "./metaMaskReducer";
 import saleReducer from "./saleReducer";
@@ -9,6 +9,6 @@ export default combineReducers({
   art: artReducer,
   auth: userReducer,
   MetaMask: metaMaskReducer,
-  UI: uiReducer,
+  UI: UIReducer,
   sales: saleReducer,
 });
