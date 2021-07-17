cc4f91bbf4fab1dfd7bb2e3fe890c42219c860c3
Colin Schlecht <schlechtcolin@yahoo.com>
2021-07-17 15:08:56 -0700

    adds admin option in reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,16 +1,32 @@
-import { /*GET_USER, SIGN_IN, SIGN_OUT,*/ SIGN_UP, UPDATE_USER } from "../actions/types";
+import {
+  /*GET_USER, SIGN_IN, SIGN_OUT,*/ SIGN_UP,
+  UPDATE_USER,
+} from "../actions/types";
 
 const INITIAL_STATE = {
   isSignedIn: false,
   user: null,
+  isAdmin: false,
 };
+
+
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SIGN_UP:
-      return { ...state, isSignedIn: true, user: action.payload };
+      return {
+        ...state,
+        isSignedIn: true,
+        user: action.payload,
+        isAdmin: action.payload.user.metamask_account === "0x71e79463868d9f56a805ec82db59c3c848693eb1",
+      };
     case UPDATE_USER:
-      return { ...state, isSignedIn: true, user: action.payload };
+      return {
+        ...state,
+        isSignedIn: true,
+        user: action.payload,
+        isAdmin: action.payload.user.metamask_account === "0x71e79463868d9f56a805ec82db59c3c848693eb1",
+      };
     // case SIGN_IN:
     //   return { ...state, isSignedIn: true, user: action.payload };
     // case SIGN_OUT:
